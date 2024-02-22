74b3bec37e5b7f63359019d4a6aa7c864768136a
Tigu Ugur <u.tigu@softfair.de>
2024-02-22 10:15:27 +0100

    all commits in one edited

---
+++ someFIle.ts
@@ -0,0 +1,15 @@
+export const someFunction = () => {
+  return "Hello World changed this 1";
+};
+
+export const someOtherFunction = () => {
+  return "Hello World 3";
+};
+
+export const someOtherFunction2 = () => {
+  return "Hello World 4";
+};
+
+export const someOtherFunction3 = () => {
+  return "Hello World 5";
+};
